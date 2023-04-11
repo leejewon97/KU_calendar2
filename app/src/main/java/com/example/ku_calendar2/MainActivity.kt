9933ca326a82c7d35e9438162d2e05c1cc1869a6
Jtility <leejewon97@gmail.com>
2023-04-12 08:28:47 +0900

    spinner 완료 spinner select시 listner 구현 완료 해당 월의 1일이 위치한 곳의 index(position)을 계산하여 recyclerView를 scroll index 계산을 수월히 하기 위해, dates 생성 후 초기화 정상 작동 확인

--- app/src/main/java/com/example/ku_calendar2/MainActivity.kt
+++ app/src/main/java/com/example/ku_calendar2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.ku_calendar2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ku_calendar2.databinding.ActivityMainBinding
 import java.util.*
 import kotlin.collections.ArrayList
@@ -14,14 +17,55 @@ import kotlin.collections.ArrayList
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     var datas:ArrayList<MyData> = ArrayList()
-    lateinit var adapter:MyDataAdapter
+    var dates:ArrayList<String> = ArrayList()
+    lateinit var dataAdapter:MyDataAdapter
+    lateinit var spinnerAdapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initData()
         initRecyclerView()
+        initSpinner()
     }
+
+    fun initSpinner() {
+        spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ArrayList<String>())
+        spinnerAdapter.add("9")
+        spinnerAdapter.add("10")
+        spinnerAdapter.add("11")
+        spinnerAdapter.add("12")
+        spinnerAdapter.add("1")
+        spinnerAdapter.add("2")
+        binding.spinner.adapter = spinnerAdapter
+
+        val smoothScroller: RecyclerView.SmoothScroller by lazy {
+            object : LinearSmoothScroller(this) {
+                override fun getVerticalSnapPreference() = SNAP_TO_START
+            }
+        }
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val month = parent?.getItemAtPosition(position).toString()
+                smoothScroller.targetPosition = calculatePosition(month)
+                binding.recyclerView.layoutManager?.startSmoothScroll(smoothScroller)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+    }
+
+    fun calculatePosition(month: String): Int {
+        return dates.indexOf("${month}_1")
+    }
+
     fun initData() {
         val scan = Scanner(resources.openRawResource(R.raw.info))
         while (scan.hasNextLine()){
@@ -29,20 +73,22 @@ class MainActivity : AppCompatActivity() {
             val date = scan.next()
             val schedule = scan.nextLine().substring(1)
             datas.add(MyData(month, date, schedule, false))
+            dates.add("${month}_${date}")
         }
         Log.d("datas", "$datas")
     }
+
     fun initRecyclerView() {
         binding.recyclerView.layoutManager = GridLayoutManager(this, 7)
-        adapter = MyDataAdapter(datas)
-        adapter.itemClickListener = object : MyDataAdapter.OnItemClickListener {
+        dataAdapter = MyDataAdapter(datas)
+        dataAdapter.itemClickListener = object : MyDataAdapter.OnItemClickListener {
             override fun onItemClick(data: MyData, adapterPosition: Int) {
                 if (data.schedule != "") {
                     data.isOpen = !(data.isOpen)
-                    adapter.notifyItemRangeChanged(0, adapterPosition + 7)
+                    dataAdapter.notifyItemRangeChanged(0, adapterPosition + 7)
                 }
             }
         }
-        binding.recyclerView.adapter = adapter
+        binding.recyclerView.adapter = dataAdapter
     }
 }
